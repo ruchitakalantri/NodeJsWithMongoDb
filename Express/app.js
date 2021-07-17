15f76b07b84ad2803d3a6cec85c57ba43fccbee1
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-17 18:04:04 +0530

    npm install body parser

--- Express/app.js
+++ Express/app.js
@@ -2,16 +2,23 @@ const http = require('http');
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express(); //Creates an Express application.
 
+//parser
+app.use(bodyParser.urlencoded({extended : false})); //Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 
 app.use('/add-product' , (req, res, next) => { 
-    console.log('In add product middleware!!!');
-    res.send('<h1> Add product!!!</h1>');//Send a response.
+    res.send('<form action="/product" method = "POST"><input type ="text" name="title"> <button type = "submit"> Add Product </button></form>');//Send a response.
+});
+
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');   
 });
 
 app.use('/' , (req, res, next) => { 
-    console.log('In middleware!!!');
     res.send('<h1> Hello from express!!!</h1>');//Send a response.
 });
 
