6f785454d19072817b35964915acaf41e7f25e30
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-17 18:07:36 +0530

    limiting middleware for post request

--- Express/app.js
+++ Express/app.js
@@ -13,7 +13,7 @@ app.use('/add-product' , (req, res, next) => {
     res.send('<form action="/product" method = "POST"><input type ="text" name="title"> <button type = "submit"> Add Product </button></form>');//Send a response.
 });
 
-app.use('/product',(req,res,next) => {
+app.post('/product',(req,res,next) => {
     console.log(req.body);
     res.redirect('/');   
 });
