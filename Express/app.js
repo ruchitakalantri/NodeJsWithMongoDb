b08ff20911dcaf32fff390068d379cba3b95ac6d
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-17 17:05:55 +0530

    more about  middleware for express project

--- Express/app.js
+++ Express/app.js
@@ -15,6 +15,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => { 
 
     console.log('in another middleware!!!');
+    
+    res.send('<h1> Hello from express!!!</h1>');//Send a response.
 
 } );
 
