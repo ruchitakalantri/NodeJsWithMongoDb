88150f347f499988781053e73a28df5b005b8a47
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-17 17:41:49 +0530

    use of app.use and its order for middleware

--- Express/app.js
+++ Express/app.js
@@ -5,18 +5,14 @@ const express = require('express');
 const app = express(); //Creates an Express application.
 
 
-app.use((req, res, next) => { 
+app.use('/add-product' , (req, res, next) => { 
+    console.log('In add product middleware!!!');
+    res.send('<h1> Add product!!!</h1>');//Send a response.
+});
 
-    console.log('in middleware!!!');
-    next(); // allows the request to conntinue to the next middleware in line
-
-} );//add new middlewarre function
-
-app.use((req, res, next) => { 
-
-    console.log('in another middleware!!!');
+app.use('/' , (req, res, next) => { 
+    console.log('In middleware!!!');
     res.send('<h1> Hello from express!!!</h1>');//Send a response.
-
 });
 
 app.listen(3000); //Listen for connections.A node http.Server is returned, with this application (which is a Function) as its callback. 
