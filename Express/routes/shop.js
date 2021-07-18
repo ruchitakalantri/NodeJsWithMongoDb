c8bff38abab1c458557a9174e1c040bedaf210a4
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-18 10:59:21 +0530

    add path to root file by useing export

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -7,7 +9,7 @@ const router = express.Router();
 router.get('/' , (req, res, next) => { 
     //__dirname : hold absolute path for this project folder : routes folder
     // path join : Join all arguments together
-    res.sendFile(path.join(__dirname ,'../', 'views' , 'shop.html')); //Transfer the file at the given path.
+    res.sendFile(path.join(rootDir, 'views' , 'shop.html')); //Transfer the file at the given path.
 });
 
 module.exports = router;
