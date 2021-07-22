a3adacc77ffc9c0e670702fc0ac467c1066d107e
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-22 17:39:59 +0530

    add sequelize module and made connection to database

--- Express/util/database.js
+++ Express/util/database.js
@@ -1,13 +1,23 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-// connection pool : pool of connetion.. multiple connection 
-const pool = mysql.createPool({
-    //db engine
-    host : 'localhost',
-    user : 'root',
-    database : 'node-complete',
-    password : 'password'
-});
+// // connection pool : pool of connetion.. multiple connection 
+// const pool = mysql.createPool({
+//     //db engine
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'node-complete',
+//     password : 'password'
+// });
 
-// promise : callback inn struture way
-module.exports = pool.promise();
+// // promise : callback inn struture way
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+// new sequelize object : will atomatically connect to database
+const sequelize = new Sequelize('node-complete' , 'root' , 'password' , {
+    dialect : 'mysql' ,
+     host : 'localhost'
+    });
+
+    module.exports = sequelize;
