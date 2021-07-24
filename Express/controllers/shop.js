2c269cc4bfde045dc6a066ae634543b532fab796
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-24 12:23:05 +0530

    adding existing products and retriveing cart item

--- Express/controllers/shop.js
+++ Express/controllers/shop.js
@@ -81,42 +81,38 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
-
+  let newQuantity = 1;
   req.user
     .getCart()
     .then(cart => {
       fetchedCart = cart;
-      return cart.getProducts({ where : {id: prodId}});
+      return cart.getProducts({ where: { id: prodId } });
     })
     .then(products => {
       let product;
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
-      if(product) {
-        //...
+
+      if (product) {
+        ////Add product to cart which is already part of cart 
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      // no product
-      return Product.findByPk(prodId)
-        .then(product => {
-          return fetchedCart.addProduct(product , 
-            { through : { quantity : newQuantity}
-          });
-        })
-        .catch(err => console.log(err))
-      
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
     })
     .then(() => {
-      res.redirect("/cart");
+      res.redirect('/cart');
     })
     .catch(err => console.log(err));
-
-  // Product.findById(prodId, (product) => {
-  //   Cart.addProduct(prodId, product.price);
-  // });
-  // res.redirect("/cart");
 };
+
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
