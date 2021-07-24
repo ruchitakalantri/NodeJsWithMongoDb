32f5d47b32b333a4a88debb50d61687e7391cb01
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-24 15:44:00 +0530

    adding order module

--- Express/app.js
+++ Express/app.js
@@ -11,6 +11,9 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 
 const app = express();
 
@@ -50,6 +53,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product , {through : CartItem});
 Product.belongsToMany(Cart , {through : CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through : OrderItem});
 
 // sync your model to database
 //sync({force : true})  forcefully reset all table in database
