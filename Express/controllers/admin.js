b161dad74b81999eb878f36744a39a3efc7d05d6
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-24 10:45:23 +0530

    making use of sequelize association for fetching product

--- Express/controllers/admin.js
+++ Express/controllers/admin.js
@@ -40,10 +40,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({where : {id : prodId}})
+    //Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0] ;
       if (!product) {
+        
         return res.redirect('/');
       }
       res.render('admin/edit-product', {
@@ -82,7 +85,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products' , {
         prods : products,
@@ -107,6 +111,4 @@ exports.postDeleteProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
-
-  
 };
