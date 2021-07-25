482b4d7b3f8a894f37436acb141ebbc8e8f2949d
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-25 12:23:57 +0530

    deleted other assignment folders

--- Assignment2/util/path.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-//deprecation warning for that code 
-//module.exports = path.dirname(process.mainModule.filename)
-
-module.exports = path.dirname(require.main.filename);
-
-// TO GET PATH TO THAT DIRECTORY
