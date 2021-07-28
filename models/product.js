fc8a230a54357eb77d75e2293e5ea082927cf4bf
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-28 11:17:45 +0530

    fetching single  product from database

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -32,6 +33,21 @@ class Product {
         return products;
       })
       .catch(err => console.log(err));
+  }
+
+  static findById(prodId) {
+    // here _id is objectId type 
+    // mongo db stores id in _id 
+    const db = getDb();
+    return db.collection('products')
+      .find({_id : new mongodb.ObjectID(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => console.log(err));
+
   }
 
 }
