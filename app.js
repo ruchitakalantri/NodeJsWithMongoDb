36b7807d6e12b485883d6610fe50cd14209a5768
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-30 12:17:37 +0530

    adding add to cart functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('610102ad14f2d251faf11564')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name , user.email , user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectID;
 
 class User {
-  constructor(username , email) {
+  constructor(username , email , cart , id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items : []}
+    this._id = id;
   }
 
   save() {
@@ -15,6 +17,24 @@ class User {
       .collection('users')
       .insertOne(this);
 
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp.ObjectID-id === product._id
+    // });
+
+    // add product to cart
+    //product.quantity = 1
+    // ... : JS spread operator 
+    // overwrite old cart to new cart
+    const updatedCart = {items : [{...product , quantity : 1 }]};
+    const db = getDb();
+    return db.collection('users')
+      .updateOne(
+        { _id : new ObjectId(this._id)} , 
+        { $set : {cart : updatedCart}}
+      );
   }
 
   // .. find will give curser
